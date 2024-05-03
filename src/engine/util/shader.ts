ba91d95d9a9469a1c0ee130956941a9b8418b740
larsjarlvik <lars.jarlvik@gmail.com>
2024-05-03 22:19:49 +0700

    Use relative URLs to support github pages

--- src/engine/util/shader.ts
+++ src/engine/util/shader.ts
@@ -5,7 +5,7 @@ export interface Shader {
 }
 
 export const fetchShader = async (name: string): Promise<Shader> => {
-    const response = await fetch(`/shaders/${name}.wgsl`);
+    const response = await fetch(`shaders/${name}.wgsl`);
     const shader = await response.text();
 
     const vert = shader.substring(0, shader.indexOf('// Fragment'));
